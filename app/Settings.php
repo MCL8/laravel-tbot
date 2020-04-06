a94c801ea2c59ef412bb46479e7721c67d7b8115
Igor F <ig.freid@gmail.com>
2020-04-06 16:40:31 +0600

    Settings->getSettings added

--- app/Settings.php
+++ app/Settings.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Settings extends Model
 {
-    //
+    public $timestamps = false;
+
+    public static function  getSettings($key = null)
+    {
+        $settings = $key ? self::where('key', $key)->first() : self::get();
+
+        $collect = collect();
+
+        foreach ($settings as $setting) {
+            $collect->put($setting->key, $setting->value);
+        }
+
+        return $collect;
+    }
 }
